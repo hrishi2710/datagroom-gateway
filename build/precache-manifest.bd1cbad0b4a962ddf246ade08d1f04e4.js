b1efc2d61f50204dbb3a8a7aa9da5e8a5d61b200
Hrishikesh Kumar <hkumar@infinera.com>
2022-10-29 14:00:44 +0530

    Added UI build

---
+++ build/precache-manifest.bd1cbad0b4a962ddf246ade08d1f04e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fde3c8250bfbc98afc519f476a03197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afa2f9c9612b3e68942d",
+    "url": "/static/css/2.84dd17c5.chunk.css"
+  },
+  {
+    "revision": "1498699ce15ce431dd98",
+    "url": "/static/css/main.6a3eb87f.chunk.css"
+  },
+  {
+    "revision": "afa2f9c9612b3e68942d",
+    "url": "/static/js/2.8d76bb12.chunk.js"
+  },
+  {
+    "revision": "1498699ce15ce431dd98",
+    "url": "/static/js/main.a300e597.chunk.js"
+  },
+  {
+    "revision": "389267fd9b59b47804cb",
+    "url": "/static/js/runtime~main.ce518ed5.js"
+  }
+]);
